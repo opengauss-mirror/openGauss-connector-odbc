b5cc251740c6004e74043d18f97ccc40b33349a7
zhangxubo <bxuzhang@163.com>
2025-11-13 20:16:52 +0800

    update version to 6.0.3

--- build.sh
+++ build.sh
@@ -59,7 +59,7 @@ fi
 
 ##default install version storage path
 declare mppdb_name='openGauss-ODBC'
-declare version_number='6.0.2'
+declare version_number='6.0.3'
 #######################################################################
 ## print help information
 #######################################################################
